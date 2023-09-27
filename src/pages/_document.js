31e072b4bca84dd5a2a7920162b8f99e1579f13f
Иванов Даниил <80471755+Danilis-fila@users.noreply.github.com>
2023-09-27 22:40:11 +0300

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,12 +2,24 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className='body'>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+    <>
+     <Head>
+        <title>О компании</title>
+        <meta name="description" content="Крым-Кровля, Контактное лицо, Телефон, Telegram, Viber, WhatsApp, Контакты, "/>
+        <meta name="keywords" content="Крым-Кровля, крым кровля, ООО Крым Кровля, ПВХ мембрана Крым, ПВХ мембрана Симферополь, Плоская кровля Симферополь, 
+                                      Аэратор Симферополь, Дорожки ПВХ, Мембрана plastfoil пластфоил, Гидроизоляция фундамента, Гидроизоляция кровли ПВХ,
+                                      ПВХ кровля крыша, Гидроизоляция террасы, ПВХ воронки Крым, Воронки для плоской кровли, ПВХ металл, Крым Кровля ПВХ,
+                                      Ремонт ПВХ Крым, Ремонт плоской кровли крыши, Ремонт битумной кровли, Дорожки для плоской крыши, Эксплуатируемая кровля"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="icon" href="image/logo-roof.svg"/>
+      </Head>
+      <Html lang="en">
+        <Head />
+        <body className='body'>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    </>
   )
 }
